9229b57db3c86080a87c79673938629def4befb7
CKShetty4 <ckshetty004@gmail.com>
2025-01-23 08:47:11 +0530

    add : TextProcessing -Medium : Paste

---
+++ Text Processing/Medium/Paste3.sh
@@ -0,0 +1,3 @@
+# The delimiter between consecutive rows of data has been transformed from the newline to a tab. 
+# The delimiter option is not necessary as tab is the delimiter of paste by default
+paste -s 
